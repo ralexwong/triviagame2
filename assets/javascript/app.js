863119fea15a3477959b761a26ff7191fe1108e4
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-04-25 16:00:04 -0700

    added images to triviaQuestions array. working on a way for the code to detect what option the user selects. Might re work the html a bit and make it contructed more on javascript/jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,53 @@
 // create variables for answer count
 var correct = 0;
-var timer = 30;
+var incorrect = 0;
 
 
 
 
-// array to hold questions
 
-var triviaQuestions = {
-    "Who played Noah in the movie 'The Notebook'?":
-    "Which 2005 film's tagline is 'Better Late Than Never'?",
-    "In what U.S. city does the action take place in 'The Departed'?",
-    "In 'The Godfather', what does film producer Jack Woltz discover in his bed?",
-    "Which film ended with real people and their movie counterparts placing rocks on a grave?",
-    "An alien attack destroyed the White House in what 1996 summer blockbuster?",
-    "Which film opened on a man strutting down the street, paint can in hand, to a song by the Bee Gees?",
-    "The opening scene of 'Saving Private Ryan' was a re-enactment of what World War II battle?",
-    "In 'The Incredibles', what did Bob Parr do to get fired from his job?"
-}
+// array to hold questions/answers/images as each an object so they are all contained within its group
+// 
+
+var triviaQuestions = [{
+    question: "What was the top-grossing film for 2002?",
+    answers: ["Spider Man", "Signs", "Star Wars: Episode II", "My Big Fat Greek Wedding"],
+    correctAnswer: "Spider Man",
+    images: "../imges/spiderman.jpg"},
+    {
+    question: "Who played Noah in the movie 'The Notebook'?",
+    answers: ["Ryan Phillipe", "Ryan Gossling", "Freddit Prinze Jr.", "Colin Farrell"],
+    correctAnswer: "Ryan Gossling",
+    images: "../imges/ryangossling.jpg"},
+    {
+    question: "Which 2005 film's tagline is 'Better Late Than Never'?",
+    answers: ["The 40 Year-Old Virgin", "Fantastic Four", "The Hitchhiker's Guide to the Galaxy", "The Ring"],
+    correctAnswer: "The 40 Year-Old Virgin",
+    images: "../imges/virgin.jpg"},
+    {
+    question: "In what U.S. city does the action take place in 'The Departed'?",
+    answers: ["Beverly Hills", "Las Vegas", "Boston", "Miami"],
+    correctAnswer: "Boston",
+    images:"../imges/boston.jpg" },
+    {
+    question: "In 'The Godfather', what does film producer Jack Woltz discover in his bed?",
+    answers: ["Dead Prostitute", "Sack of Money", "Python", "Horse's Head"],
+    correctAnswer: "Horse's Head",
+    images: "../imges/horse.jpg"},
+    {
+    question: "Which film ended with real people and their movie counterparts placing rocks on a grave?",
+    answers: ["Schindler's List", "Pearl Harbor", "Saving Private Ryan", "The Pianist"],
+    correctAnswer: "Schindler's List",
+    images: "../imges/list.jpg"},
+    {
+    question: "An alien attack destroyed the White House in what 1996 summer blockbuster?",
+    answers: ["The Day After Tomorrow", "Signs", "Armageddon", "Independence Day"],
+    correctAnswer: "Independence Day",
+    images: "../imges/alien.jpg"},
+]
+
+
+
 
 
 // hides quiz before the game starts
@@ -27,11 +57,31 @@ $(document).ready(function(){
 });
 
 
-// shows the quiz, hides the start page, and starts the timer
+// shows the quiz, hides the start page
 $(document).ready(function(){
     $(".start").on("click", function() {
+        replace();
         $(".start").hide();
         $("#content").show();
+
+        $("#options").on("click", function() {
+            if($("p:contains(" + triviaQuestions[0].correctAnswer + ")")) {
+                console.log(triviaQuestions[0].correctAnswer);
+                correct++;
+                triviaQuestions.shift();
+                replace()
+            }
+            else {
+                incorrect++
+                triviaQuestions.shift();
+                replace();
+
+            }
+        });
+        
+
+
+        // if ($("#options").on("click", ))
 
 
         // have a timer that starts at 31 seconds and goes down 1 second
@@ -39,6 +89,7 @@ $(document).ready(function(){
         // when timer reaches 0 hide the quiz
         // show results
 
+        // var timer = 30;
         // exucutes the decrement function every 1 second
         // var interval = setInterval(decrement, 1000);
 
@@ -61,7 +112,16 @@ $(document).ready(function(){
     });
 });
 
+// replaces the questions and answers shown to the user.
+function replace() {
 
+    $("#question").html(triviaQuestions[0].question);
+    $("#option1").html(triviaQuestions[0].answers[0]);
+    $("#option2").html(triviaQuestions[0].answers[1]);
+    $("#option3").html(triviaQuestions[0].answers[2]);
+    $("#option4").html(triviaQuestions[0].answers[3]);
+
+}
 
 
 
@@ -75,13 +135,6 @@ function results() {
 
     $("#content").hide();
 
-    if (document.getElementById("answer1").checked === true) {
-        correct++
-        console.log(document.getElementById("answer1").checked);
-    }
-
-    // default 10 incorrect
-    incorrect = 10 - correct
 
     // displays the results page
     $("#correct").html(correct);
