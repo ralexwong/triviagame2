fe37ad624afa44239696571bccb84f15469f1eb0
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-04-27 21:14:23 -0700

    everything works but the timer event :(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,63 @@
-// create variables for answer count
-var correct = 0;
-var incorrect = 0;
+$(document).ready(function () {
+
+    // create variables for answer count
+    var correct = 0;
+    var incorrect = 0;
+
+    // array to hold questions/answers/images as each an object so they are all contained within its group
+
+    var triviaQuestions = [{
+        question: "What was the top-grossing film for 2002?",
+        answers: ["Signs", "Spider Man", "Star Wars: Episode II", "My Big Fat Greek Wedding"],
+        correctAnswer: "Spider Man",
+        images: "assets/images/spiderman.jpg"
+    },
+    {
+        question: "Who played Noah in the movie 'The Notebook'?",
+        answers: ["Ryan Phillipe", "Ryan Gossling", "Freddit Prinze Jr.", "Colin Farrell"],
+        correctAnswer: "Ryan Gossling",
+        images: "assets/images/ryangossling.jpg"
+    },
+    {
+        question: "Which 2005 film's tagline is 'Better Late Than Never'?",
+        answers: ["The 40 Year-Old Virgin", "Fantastic Four", "The Hitchhiker's Guide to the Galaxy", "The Ring"],
+        correctAnswer: "The 40 Year-Old Virgin",
+        images: "assets/images/virgin.jpg"
+    },
+    {
+        question: "In what U.S. city does the action take place in 'The Departed'?",
+        answers: ["Beverly Hills", "Las Vegas", "Boston", "Miami"],
+        correctAnswer: "Boston",
+        images: "assets/images/boston.jpg"
+    },
+    // {
+    //     question: "In 'The Godfather', what does film producer Jack Woltz discover in his bed?",
+    //     answers: ["Dead Prostitute", "Sack of Money", "Python", "Horse's Head"],
+    //     correctAnswer: "Horse's Head",
+    //     images: "assets/images/horse.jpg"
+    // },
+    // {
+    //     question: "Which film ended with real people and their movie counterparts placing rocks on a grave?",
+    //     answers: ["Schindler's List", "Pearl Harbor", "Saving Private Ryan", "The Pianist"],
+    //     correctAnswer: "Schindler's List",
+    //     images: "assets/images/list.jpg"
+    // },
+    // {
+    //     question: "An alien attack destroyed the White House in what 1996 summer blockbuster?",
+    //     answers: ["The Day After Tomorrow", "Signs", "Armageddon", "Independence Day"],
+    //     correctAnswer: "Independence Day",
+    //     images: "assets/images/alien.jpg"
+    // }
+    ]
 
 
-// array to hold questions/answers/images as each an object so they are all contained within its group
-
-var triviaQuestions = [{
-    question: "What was the top-grossing film for 2002?",
-    answers: ["Signs","Spider Man", "Star Wars: Episode II", "My Big Fat Greek Wedding"],
-    correctAnswer: "Spider Man",
-    images: "assets/images/spiderman.jpg"},
-    {
-    question: "Who played Noah in the movie 'The Notebook'?",
-    answers: ["Ryan Phillipe", "Ryan Gossling", "Freddit Prinze Jr.", "Colin Farrell"],
-    correctAnswer: "Ryan Gossling",
-    images: "assets/images/ryangossling.jpg"},
-    {
-    question: "Which 2005 film's tagline is 'Better Late Than Never'?",
-    answers: ["The 40 Year-Old Virgin", "Fantastic Four", "The Hitchhiker's Guide to the Galaxy", "The Ring"],
-    correctAnswer: "The 40 Year-Old Virgin",
-    images: "assets/images/virgin.jpg"},
-    {
-    question: "In what U.S. city does the action take place in 'The Departed'?",
-    answers: ["Beverly Hills", "Las Vegas", "Boston", "Miami"],
-    correctAnswer: "Boston",
-    images:"assets/images/boston.jpg" },
-    {
-    question: "In 'The Godfather', what does film producer Jack Woltz discover in his bed?",
-    answers: ["Dead Prostitute", "Sack of Money", "Python", "Horse's Head"],
-    correctAnswer: "Horse's Head",
-    images: "assets/images/horse.jpg"},
-    {
-    question: "Which film ended with real people and their movie counterparts placing rocks on a grave?",
-    answers: ["Schindler's List", "Pearl Harbor", "Saving Private Ryan", "The Pianist"],
-    correctAnswer: "Schindler's List",
-    images: "assets/images/list.jpg"},
-    {
-    question: "An alien attack destroyed the White House in what 1996 summer blockbuster?",
-    answers: ["The Day After Tomorrow", "Signs", "Armageddon", "Independence Day"],
-    correctAnswer: "Independence Day",
-    images: "assets/images/alien.jpg"},
-]
-
-
-
-// hides quiz before the game starts
-$(document).ready(function(){
+    // hides quiz before the game starts
     $("#content").hide();
     $("#results").hide();
-});
 
 
-// shows the quiz, hides the start page
-$(document).ready(function(){
-    $(".start").on("click", function() {
+    // shows the quiz, hides the start page
+    $(".start").on("click", function () {
 
         // inserts the question and answers into the html
         replace();
@@ -64,11 +68,28 @@ $(document).ready(function(){
         // show the timer, question, and answers
         // intitates the timer
         $("#content").show();
+    });
+
+
+    // replaces the questions and answers shown to the user.
+    function replace() {
+
+        $("#outcome").hide();
+        $("#showAnswer").hide();
+        $("#imageInput").empty();
+
+        $("#quiz").append("<p id=question>" + triviaQuestions[0].question + "</p>");
+
+        $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[0] + "'>" + triviaQuestions[0].answers[0] + "</p>");
+        $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[1] + "'>" + triviaQuestions[0].answers[1] + "</p>");
+        $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[2] + "'>" + triviaQuestions[0].answers[2] + "</p>");
+        $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[3] + "'>" + triviaQuestions[0].answers[3] + "</p>");
+        
         timer();
 
         // checks what answer the user picks
-        $(".options").on("click", function() {
-            console.log(this);
+        $(".options").on("click", function () {
+
             console.log(this.textContent);
 
             // checks what the answer is and compares it to the correct answer in the triviaQuestions array
@@ -101,85 +122,90 @@ $(document).ready(function(){
             }
 
             else {
+                console.log(triviaQuestions[0].correctAnswer);
                 clearInterval(timer);
-                incorrect++
+                incorrect++;
                 wrongAnswerScreen();
                 triviaQuestions.shift();
                 $("#quiz").empty();
                 checkArray();
                 setTimeout(replace, 3000);
                 $("#quiz").show();
-
             }
         });
-
-        
-    });
-});
-
-// replaces the questions and answers shown to the user.
-function replace() {
-
-    $("#outcome").hide();
-    $("#imageInput").empty();
-    $("#quiz").append("<p id=question>" + triviaQuestions[0].question + "</p>")
-
-    $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[0] + "'>"  + triviaQuestions[0].answers[0] + "</p>")
-    $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[1] + "'>" + triviaQuestions[0].answers[1] + "</p>")
-    $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[2] + "'>" + triviaQuestions[0].answers[2] + "</p>")
-    $("#quiz").append("<p class=options value='" + triviaQuestions[0].answers[3] + "'>" + triviaQuestions[0].answers[3] + "</p>")
-}
-
-function rightAnswerScreen() {
-    $("#quiz").hide();
-    $("#outcome").html("Correct!");
-    $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "' />");
-}
-
-function wrongAnswerScreen() {
-    $("#quiz").hide();
-    $("#outcome").html("Nope!");
-    $("#showAnswer").html("The correct answer was: " + triviaQuestions[0].correctAnswer);
-    $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "'/>");
-}
-
-function timeOutScreen() {
-    $("#showAnswer").html("The correct answer was: " + triviaQuestions[0].correctAnswer);
-    $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "'/>");
-    setTimeout(replace, 3000);
-    $("#quiz").show();
-}
-
-
-
-function checkArray() {
-    if (triviaQuestions.indexOf(0) === -1) {
-        $("#postquestion").hide();
-        results();
     }
-}
 
+    // when the user gets the right answer
+    function rightAnswerScreen() {
+        $("#quiz").hide();
+        $("#outcome").html("Correct!");
+        $("#outcome").show();
+        $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "' />");
+    }
 
-function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('timerNumber').innerHTML= sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-            timeOutScreen();
+    // when user gets the wrong answer
+    function wrongAnswerScreen() {
+        $("#quiz").hide();
+        $("#outcome").html("Nope!");
+        $("#outcome").show();
+        $("#showAnswer").html("The correct answer was: " + triviaQuestions[0].correctAnswer);
+        $("#showAnswer").show();
+        $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "'/>");
+    }
+
+    // when user runs out of time
+    function timeOutScreen() {
+        $("#outcome").html("Out of Time!");
+        $("#outcome").show();
+        $("#showAnswer").html("The correct answer was: " + triviaQuestions[0].correctAnswer);
+        $("#showAnswer").show();
+        $("#imageInput").append("<img id='image' src='" + triviaQuestions[0].images + "'/>");
+    }
+
+    // when the user runs of time
+    function timedOut() {
+        console.log(triviaQuestions[0].correctAnswer);
+        clearInterval(timer);
+        incorrect++;
+        triviaQuestions.shift();
+        $("#quiz").empty();
+        checkArray();
+        setTimeout(replace, 3000);
+        $("#quiz").show();
+    }
+
+    // if the triviaQuestions array is empty (no more questions), then it will post the results
+    function checkArray() {
+        if (triviaQuestions.length === 0) {
+            $("#postquestion").hide();
+            results();
         }
-    }, 1000);
-}
+    }
+
+    // timer function
+    function timer() {
+
+        var sec = 30;
+        var timer = setInterval(function () {
+            document.getElementById('timerNumber').innerHTML = sec;
+            sec--;
+            if (sec < 0) {
+                clearInterval(timer);
+                timeOutScreen();
+                timedOut();
+            }
+        }, 1000);
+    }
+
+    // displays the results for the user
+    function results() {
+
+        $("#content").hide();
+
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        $("#results").show();
+    }
 
 
-
-// displays the results for the user
-function results() {
-
-    $("#content").hide();
-
-    $("#correct").html(correct);
-    $("#incorrect").html(incorrect);
-    $("#results").show();
-}
+});
